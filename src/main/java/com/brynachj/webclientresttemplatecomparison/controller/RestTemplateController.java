238ff07856971ee9abbe6ae4e4841872ac3b9818
Brynach Jones <brynach.jones@gmail.com>
2020-01-06 23:09:06 +0000

    Controllers and Services set up to show off examples

---
+++ src/main/java/com/brynachj/webclientresttemplatecomparison/controller/RestTemplateController.java
@@ -0,0 +1,51 @@
+package com.brynachj.webclientresttemplatecomparison.controller;
+
+import com.brynachj.webclientresttemplatecomparison.gateway.RestTemplateGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/restTemplate")
+public class RestTemplateController {
+
+    @Autowired
+    private RestTemplateGateway restTemplateGateway;
+
+    @GetMapping("/tea")
+    public String getTea() {
+
+        String mug = restTemplateGateway.getMug();
+        String teabag = restTemplateGateway.getTeabag();
+        String hotWater = restTemplateGateway.getHotWater();
+        String milk = restTemplateGateway.getMilk();
+
+        String[] responses = {mug, teabag, hotWater, milk, "tea has been made the restTemplate way"};
+        return String.join("; ", responses);
+    }
+
+    @GetMapping("/tea/mug")
+    public String getMug() {
+
+        return restTemplateGateway.getMug();
+    }
+
+    @GetMapping("/tea/teabag")
+    public String getTeabag() {
+
+        return restTemplateGateway.getTeabag();
+    }
+
+    @GetMapping("/tea/hot-water")
+    public String getHotWater() {
+
+        return restTemplateGateway.getHotWater();
+    }
+
+    @GetMapping("/tea/milk")
+    public String getMilk() {
+
+        return restTemplateGateway.getMilk();
+    }
+}
---
+++ src/main/java/com/brynachj/webclientresttemplatecomparison/controller/TeaController.java
@@ -0,0 +1,42 @@
+package com.brynachj.webclientresttemplatecomparison.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tea")
+public class TeaController {
+
+    public static final Logger logger = LoggerFactory.getLogger(TeaController.class);
+
+    @GetMapping("/mug")
+    public String getMug() throws InterruptedException {
+        logger.info("fetching a mug");
+        Thread.sleep(1000);
+        return "got a mug";
+    }
+
+    @GetMapping("/teabag")
+    public String getTeabag() throws InterruptedException {
+        logger.info("fetching a teabag");
+        Thread.sleep(1000);
+        return "got a teabag";
+    }
+
+    @GetMapping("/milk")
+    public String getMilk() throws InterruptedException {
+        logger.info("fetching milk");
+        Thread.sleep(1000);
+        return "got milk";
+    }
+
+    @GetMapping("/hot-water")
+    public String getHotWater() throws InterruptedException {
+        logger.info("fetching hot-water");
+        Thread.sleep(1000);
+        return "got hot water";
+    }
+}
