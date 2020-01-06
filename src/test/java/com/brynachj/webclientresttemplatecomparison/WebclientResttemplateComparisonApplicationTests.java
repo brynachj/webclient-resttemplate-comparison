694b44e72d7869341d058397b539b4fb32a413f4
Brynach Jones <brynach.jones@gmail.com>
2020-01-06 20:41:19 +0000

    first commit

---
+++ src/test/java/com/brynachj/webclientresttemplatecomparison/WebclientResttemplateComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brynachj.webclientresttemplatecomparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientResttemplateComparisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
